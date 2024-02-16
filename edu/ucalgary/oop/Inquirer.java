a0b8291c770c4c867a6dacba6b25556723431d29
Sabrina <144979434+realvegetablegummy@users.noreply.github.com>
2024-02-16 16:40:05 -0700

    Update Inquirer.java
    changed getter methods to camel case, kept var names as upper case

--- edu/ucalgary/oop/Inquirer.java
+++ edu/ucalgary/oop/Inquirer.java
@@ -1,8 +1,5 @@
 package edu.ucalgary.oop;
 
-import java.security.PrivateKey;
-import java.security.PublicKey;
-
 public class Inquirer {
     private String FIRST_NAME;
     private String LAST_NAME;
@@ -17,8 +14,8 @@ public class Inquirer {
         this.INFO = INFO;
     }
 
-    public String getFIRST_NAME(){return this.FIRST_NAME;}
-    public String getLAST_NAME(){return this.LAST_NAME;}
-    public String getINFO(){return this.INFO;}
-    public String getSERVICES_PHONE(){return this.SERVICES_PHONE;}
+    public String getFirstName(){return this.FIRST_NAME;}
+    public String getLastName(){return this.LAST_NAME;}
+    public String getInfo(){return this.INFO;}
+    public String getServicesPhoneNum(){return this.SERVICES_PHONE;}
 }
